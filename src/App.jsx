7270601d903896a263e7e4a5b6c0a69c7d5543d4
ChangWynn <huynhchang.one@gmail.com>
2023-06-27 23:57:28 +0100

    added trackbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,52 @@
 import { useEffect, useRef, useState } from "react";
 import styles from "./App.module.css";
 import audioFiles from "./assets/audioFiles";
+import { motion } from "framer-motion";
 
 const App = () => {
   const [songIsPlaying, setSongIsPlaying] = useState(false);
   const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const [songProgression, setSongProgression] = useState(0);
+  const [bufferProgression, setBufferProgression] = useState(0);
 
   const currentSong = useRef();
 
-  console.log("currentsong", audioFiles.length);
+  const updateBufferProgression = ({ buffered, duration }) => {
+    if (buffered.length > 0) {
+      for (let i = 0; i < buffered.length; i++) {
+        const bufferProgression = (buffered.end(i) / duration) * 100;
+        setBufferProgression(bufferProgression);
+      }
+    }
+  };
+
+  const updateSongProgression = ({ currentTime, duration }) => {
+    const songProgress = ((currentTime / duration) * 100).toFixed(2);
+    setSongProgression(songProgress);
+  };
 
   useEffect(() => {
-    if (songIsPlaying) currentSong.current.play();
-    else currentSong.current.pause();
+    const song = currentSong?.current;
+
+    const updateProgressBar = () => {
+      updateSongProgression(song);
+      updateBufferProgression(song);
+    };
+
+    let interval;
+    if (songIsPlaying) {
+      song.play();
+      interval = setInterval(updateProgressBar, 1000);
+    } else {
+      song.pause();
+    }
+
+    return () => {
+      return clearInterval(interval);
+    };
   }, [songIsPlaying, currentSongIndex]);
 
+  // playlist navigation
   const togglePlay = () => {
     setSongIsPlaying(!songIsPlaying);
   };
@@ -39,12 +71,40 @@ const App = () => {
     }
   };
 
+  // click on track
+  const navigateInSong = (e) => {
+    const width = e.target.clientWidth;
+    const clickOffset = e.nativeEvent.offsetX;
+    const newCurrentTime = (clickOffset / width) * currentSong.current.duration;
+    const newSongProgress = (clickOffset / width) * 100;
+
+    currentSong.current.currentTime = newCurrentTime;
+    setSongProgression(newSongProgress);
+  };
+
   return (
     <div className={styles["app"]}>
       <audio
         ref={currentSong}
         src={audioFiles[currentSongIndex].track()}
+        onEnded={nextSong}
       ></audio>
+      <div className={styles["track--container"]}>
+        <div className={styles["track--background"]}>
+          <div
+            onClick={navigateInSong}
+            className={styles["track--overlay"]}
+          ></div>
+          <div
+            style={{ width: bufferProgression + "%" }}
+            className={styles["track--buffer"]}
+          ></div>
+          <div
+            style={{ width: songProgression + "%" }}
+            className={styles["track--progress"]}
+          ></div>
+        </div>
+      </div>
       <button onClick={previousSong}>Prev</button>
       <button onClick={togglePlay}>{songIsPlaying ? "Pause" : "Play"}</button>
       <button onClick={nextSong}>Next</button>
