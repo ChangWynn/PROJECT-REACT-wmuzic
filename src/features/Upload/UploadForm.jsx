1edeec6b2cd77c9dfbbb7e822e18941f585a8ce8
ChangWynn <huynhchang.one@gmail.com>
2023-07-02 19:44:31 +0100

    implemented click on backdrop close modal

--- src/features/Upload/UploadForm.jsx
+++ src/features/Upload/UploadForm.jsx
@@ -2,9 +2,15 @@ import styles from "./UploadForm.module.css";
 import axios from "../../services/axios";
 import { apiKey } from "../../.api";
 import { Context } from "../App/MusicPlayer";
-import useUploadError from "../hooks/useUploadState";
+import useUploadState from "../hooks/useUploadState";
 
-import React, { useContext, useEffect, useRef, useState } from "react";
+import React, {
+  forwardRef,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import { useOutletContext } from "react-router-dom";
 
 import { storage } from "../../config/firebase";
@@ -12,8 +18,8 @@ import { ref, uploadBytes, updateMetadata } from "firebase/storage";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAlbum } from "@fortawesome/pro-solid-svg-icons";
 
-const UploadForm = ({ showForm, setShowForm }) => {
-  const { uploadState, errorState, triggerUploadError } = useUploadError();
+const UploadForm = ({ showForm, setShowForm }, { dialogRef }) => {
+  const { isUploading, uploadState, uploadError } = useUploadState();
 
   const { uid } = useOutletContext();
   const { songRefs, setSongRefs } = useContext(Context);
@@ -25,6 +31,7 @@ const UploadForm = ({ showForm, setShowForm }) => {
   const titleRef = useRef();
   const artistRef = useRef();
   const fileInputRef = useRef();
+  const backdropRef = useRef();
 
   useEffect(() => {
     if (uploadedSong) {
@@ -40,15 +47,16 @@ const UploadForm = ({ showForm, setShowForm }) => {
 
   const addNewSong = async (e) => {
     e.preventDefault();
-    uploadState.setIsUploading(true);
-    const title = titleRef.current.value;
-    const artist = artistRef.current.value;
+    isUploading.set(true);
+    const title = titleRef.current.value.trim();
+    const artist = artistRef.current.value.trim();
 
     if (!title || !artist || !uploadedSong) {
-      triggerUploadError({
+      uploadError.set({
         description: "Invalid input",
         action: "Please fill in all fields",
       });
+      isUploading.stop();
       return;
     }
 
@@ -57,13 +65,24 @@ const UploadForm = ({ showForm, setShowForm }) => {
       songData = await fetchSongData(title, artist);
     }
 
-    const missingMBID = songData?.artist?.mbid || songData?.mbid;
+    const missingMBID = // returns
+      !songData?.artist?.mbid || !songData?.mbid || !songData?.album?.mbid;
 
-    if (isChecked && (missingMBID === undefined || !songData)) {
-      triggerUploadError({
-        description: "Track not found",
+    if (isChecked && !songData) {
+      uploadError.set({
+        description: "No match found",
         action: "Please check spelling or continue without LastFm metadata",
       });
+      isUploading.stop();
+      return;
+    }
+
+    if (isChecked && missingMBID) {
+      uploadError.set({
+        description: "LastFM incomplete data",
+        action: "",
+      });
+      isUploading.stop();
       return;
     }
 
@@ -79,13 +98,14 @@ const UploadForm = ({ showForm, setShowForm }) => {
       setSongRefs((prevRefs) => {
         return [...prevRefs, songRef];
       });
-      cleanUp();
     } catch (err) {
-      triggerUploadError({
+      uploadError.set({
         description: "Something went wrong",
         action: "please try again",
       });
+      isUploading.stop();
     }
+    cleanUp();
   };
 
   ////////////////////////////
@@ -118,18 +138,25 @@ const UploadForm = ({ showForm, setShowForm }) => {
   };
 
   const cleanUp = (e) => {
-    e.preventDefault();
+    if (e) e.preventDefault();
     titleRef.current.value = "";
     artistRef.current.value = "";
     fileInputRef.current.value = null;
-    uploadState.setIsUploading(false);
-    errorState.setMessage("");
+    isUploading.set(false);
+    uploadState.set("");
+    uploadError.set("");
     setUploadedSong(null);
     setShowForm(false);
   };
 
+  const closeModal = (e) => {
+    if (e.target === backdropRef.current) setShowForm(false);
+  };
+
   return (
     <div
+      ref={backdropRef}
+      onClick={closeModal}
       className={`${styles["form--background"]} ${
         showForm && styles["visible"]
       }`}
@@ -139,8 +166,8 @@ const UploadForm = ({ showForm, setShowForm }) => {
         className={`${styles["form"]} ${showForm && styles["visible"]}`}
       >
         <div className={styles["error-message"]}>
-          <h2>{errorState?.message?.description}</h2>
-          <p>{errorState?.message?.action}</p>
+          <h2>{uploadError?.message?.description}</h2>
+          <p>{uploadError?.message?.action}</p>
         </div>
         <div className={styles["form--input"]}>
           <input
@@ -194,7 +221,7 @@ const UploadForm = ({ showForm, setShowForm }) => {
             Cancel
           </button>
           <button className={styles["confirm"]}>
-            {uploadState.isUploading ? "Uploading..." : "Upload"}
+            {isUploading.state ? "Uploading..." : "Upload"}
           </button>
         </div>
       </form>
@@ -203,89 +230,3 @@ const UploadForm = ({ showForm, setShowForm }) => {
 };
 
 export default UploadForm;
-
-// export const action = async ({ request, params }) => {
-//   const data = await request.formData();
-
-//   console.log("params", params);
-
-//   const formData = {
-//     title: data.get("title"),
-//     artist: data.get("artist"),
-//     file: data.get("file"),
-//     checked: data.get("checkbox"),
-//   };
-
-//   if (!formData.title || !formData.artist || !formData.file) {
-//     return {
-//       description: "Invalid input",
-//       action: "Please fill in all fields",
-//     };
-//   }
-
-//   let songData;
-//   if (formData.checked) {
-//     songData = await fetchSongData(formData.title, formData.artist);
-//   }
-
-//   const missingMBID =
-//     songData?.artist?.mbid || songData?.album?.mbid || songData?.mbid;
-
-//   if (formData.checked && (missingMBID === undefined || !songData)) {
-//     return {
-//       description: "Track not found",
-//       action: "Please check spelling or continue without LastFm metadata",
-//     };
-//   }
-
-//   const metaData = constructMetadata(songData, formData);
-//   const songRef = ref(
-//     storage,
-//     `USER-UID-${localStorage.getItem("uid")}/${new Date()}-${
-//       metaData.customMetadata.title
-//     }-${metaData.customMetadata.artist}`
-//   );
-//   try {
-//     await uploadFile(songRef, metaData);
-//     setSongRefs((prevRefs) => {
-//       return [...prevRefs, songRef];
-//     });
-//     cleanUp();
-//   } catch (err) {
-//     console.log(err);
-//     return {
-//       description: "Something went wrong",
-//       action: "please try again",
-//     };
-//   }
-
-//   return null;
-// };
-
-// const fetchSongData = async (title, artist) => {
-//   const searchTrackURL = `/2.0/?method=track.getInfo&api_key=${apiKey}&artist=${artist}&track=${title}&format=json`;
-
-//   const res = await axios.get(searchTrackURL);
-//   return res?.data.track;
-// };
-
-// const constructMetadata = async (songData, formData) => {
-//   const userStorageRefs = await getUserStorage();
-//   const songRefs = userStorageRefs?.items;
-//   return {
-//     customMetadata: {
-//       title: songData?.name || formData.title,
-//       artist: songData?.album?.artist || formData.artist,
-//       duration: uploadedSongDuration,
-//       album: songData?.album?.title || "",
-//       imgM: songData?.album?.image[1]["#text"] || "",
-//       imgL: songData?.album?.image.at(-1)["#text"] || "",
-//       position: songRefs?.length + 1 || "",
-//     },
-//   };
-// };
-
-// const uploadFile = async (songRef, metaData) => {
-//   await uploadBytes(songRef, uploadedSong);
-//   await updateMetadata(songRef, metaData);
-// };
--- src/features/hooks/useUploadState.jsx
+++ src/features/hooks/useUploadState.jsx
@@ -2,17 +2,17 @@ import { useState } from "react";
 
 const useUploadState = () => {
   const [isUploading, setIsUploading] = useState(false);
-  const [message, setMessage] = useState(null);
-
-  const triggerUploadError = (message) => {
-    setMessage(message);
-    setIsUploading(false);
-  };
+  const [uploadState, setUploadState] = useState(null);
+  const [uploadError, setUploadError] = useState(null);
 
   return {
-    uploadState: { isUploading, setIsUploading },
-    errorState: { message, setMessage },
-    triggerUploadError,
+    isUploading: {
+      state: isUploading,
+      set: setIsUploading,
+      stop: () => setIsUploading(false),
+    },
+    uploadState: { message: uploadState, set: setUploadState },
+    uploadError: { message: uploadError, set: setUploadError },
   };
 };
 
